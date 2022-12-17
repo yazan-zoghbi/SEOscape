6d9038ed8a3878969dfc5723ff40ce63c441913b
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-12-17 05:05:09 +0200

    [MOD] fetch links route added

--- src/domain/fetch/fetch.controller.ts
+++ src/domain/fetch/fetch.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { FetchService } from './fetch.service';
 import { FetchLinkDto } from './dto/fetch-link.dto';
 
@@ -6,9 +6,20 @@ import { FetchLinkDto } from './dto/fetch-link.dto';
 export class FetchController {
   constructor(private readonly fetchService: FetchService) {}
 
-  @Post()
-  async fetch(@Body() fetchLinkDto:FetchLinkDto) {
-    console.log(fetchLinkDto.url);
-    return await this.fetchService.crawlLink(fetchLinkDto.url);
+  @Get('links')
+  async fetchLinks(
+    @Query('type') type: string,
+    @Body() fetchLinkDto: FetchLinkDto,
+  ) {
+    console.log(type);
+    if (type === 'internal') {
+      return (await this.fetchService.crawlLink(fetchLinkDto.url))
+        ['internal Links'];
+    } else if (type === 'external') {
+      return (await this.fetchService.crawlLink(fetchLinkDto.url))
+        ['external Links'];
+    } else if (type.includes('internal') && type.includes('external')) {
+      return await this.fetchService.crawlLink(fetchLinkDto.url);
+    }
   }
 }
