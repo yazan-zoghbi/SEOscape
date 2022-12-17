edc434da2f89d72994ebd291dcb5886cfce18963
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-12-17 05:04:30 +0200

    [MOD] links extractor provider imported

--- src/domain/fetch/fetch.service.ts
+++ src/domain/fetch/fetch.service.ts
@@ -1,16 +1,19 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
 import axios from 'axios';
+import { LinksExtractorService } from './links-extractor.service';
+import { utils } from 'spido';
 
 @Injectable()
 export class FetchService {
+  constructor(private readonly linksExtractorService: LinksExtractorService) {}
   async crawlLink(url: string) {
-    const responseValid = await axios.get(url).then((response) => {
-      if (response.status < 500) return true;
-    });
+    const isValid = await utils.isValidUrl(url);
 
-    if ( responseValid) {
+    if (isValid) {
+      console.log('fetching url: ' + url);
       console.log('crawling started ...');
-    }
-    else console.log('error')
+    } else console.log('error');
+
+    return await this.linksExtractorService.getLinks(url);
   }
 }
