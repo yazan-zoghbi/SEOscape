f618352329f0b975b30a4415e14343b1004f674a
Yazan Zoghbi <yazan.a.zoghbi@gmail.com>
2022-12-17 05:05:42 +0200

    [MOD] LinksExtractorService imported as provider

--- src/domain/fetch/fetch.module.ts
+++ src/domain/fetch/fetch.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { FetchController } from './fetch.controller' ;
 import { FetchService } from './fetch.service';
+import { LinksExtractorService } from './links-extractor.service';
 
 @Module({
   controllers: [FetchController],
-  providers: [FetchService],
+  providers: [FetchService, LinksExtractorService],
 })
 export class FetchModule {}
